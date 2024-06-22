422afd844c335eb365d3398333ec876f6b9747b6
GGermanBoldyrev <boldyrev_german@mail.ru>
2024-06-22 21:57:14 +0300

    Grid almost finished

--- src/core/GridGenerator.php
+++ src/core/GridGenerator.php
@@ -6,13 +6,14 @@ use GdImage;
 
 class GridGenerator
 {
+      private string $fontFile = 'fonts/Roboto-Regular.ttf';
       private int $teamsCount;
-      private int $imageWidth;
-      private int $imageHeight;
-      private int $matchWidth;
-      private int $matchHeight;
-      private int $padding;
-      private int $fontSize;
+      private int $imageWidth = 1920;
+      private int $imageHeight = 1080;
+      private int $matchWidth = 350;
+      private int $matchHeight = 125;
+      private int $padding = 50;
+      private int $fontSize = 18;
       private GdImage $image;
       private array $gridData;
 
@@ -36,37 +37,60 @@ class GridGenerator
       {
             $roundsCount = $this->calculateRoundsCount();
             $this->gridData = [];
+            $matchNumber = 1; // Счетчик матчей
 
             for ($round = 0; $round < $roundsCount; $round++) {
-                  $this->generateRoundData($round, $roundsCount);
+                  $matchNumber = $this->generateRoundData($round, $roundsCount, $matchNumber);
             }
       }
 
-      private function generateRoundData(int $round, int $roundsCount): void
+      private function generateRoundData(int $round, int $roundsCount, int &$matchNumber): int
       {
             $matchesCount = pow(2, $roundsCount - $round - 1);
             $y = $this->padding + $round * ($this->matchHeight + $this->padding) * 2;
 
             for ($matchIndex = 0; $matchIndex < $matchesCount; $matchIndex++) {
-                  $this->generateMatchData($round, $matchIndex, $y);
+                  $this->generateMatchData($round, $matchIndex, $y, $matchNumber);
+                  $matchNumber++;
             }
+
+            return $matchNumber;
       }
 
-      private function generateMatchData(int $round, int $matchIndex, int $y): void
+      private function generateMatchData(int $round, int $matchIndex, int $y, int &$matchNumber): int
       {
-            $x = $this->padding + $matchIndex * ($this->imageWidth - 2 * $this->padding) / pow(2, $this->calculateRoundsCount() - $round - 1);
-            $previousRoundMatchIndex = $matchIndex * 2;
+            // Рассчитываем горизонтальное положение матча
+            if ($round === 0) {
+                  // В первом раунде матчи располагаются равномерно
+                  $x = $this->padding + $matchIndex * ($this->imageWidth - 2 * $this->padding) / ($this->teamsCount / 2);
+            } else {
+                  // В остальных раундах матчи располагаются между предыдущими
+                  $previousRoundMatchIndex = $matchIndex * 2;
+                  $previousMatch1X = $this->gridData[$round - 1][$previousRoundMatchIndex]['x'];
+                  $previousMatch2X = $this->gridData[$round - 1][$previousRoundMatchIndex + 1]['x'];
+                  $x = ($previousMatch1X + $previousMatch2X) / 2;
+            }
 
-            $previousMatch1 = $this->getPreviousMatchNumber($round, $previousRoundMatchIndex);
-            $previousMatch2 = $this->getPreviousMatchNumber($round, $previousRoundMatchIndex + 1);
+            // Определяем номера матчей, из которых выходят участники текущего матча
+            $previousMatch1 = null;
+            $previousMatch2 = null;
+            if ($round > 0) {
+                  $previousMatch1 = $this->gridData[$round - 1][$matchIndex * 2]['matchNumber'];
+                  if ($matchIndex * 2 + 1 < count($this->gridData[$round - 1])) {
+                        $previousMatch2 = $this->gridData[$round - 1][$matchIndex * 2 + 1]['matchNumber'];
+                  }
+            }
 
+            // Добавляем данные о матче в массив
             $this->gridData[$round][] = [
-                  'matchNumber' => $matchIndex + 1 + array_sum(array_slice(range(1, $this->calculateRoundsCount() - 1), 0, $round)),
+                  'matchNumber' => $matchNumber,
                   'x' => $x,
                   'y' => $y,
                   'previousMatch1' => $previousMatch1,
                   'previousMatch2' => $previousMatch2,
             ];
+
+            return $matchNumber + 1;
       }
 
       private function getPreviousMatchNumber(int $currentRound, int $previousRoundMatchIndex): ?int
@@ -82,6 +106,43 @@ class GridGenerator
 
             foreach ($this->gridData as $round => $matches) {
                   $this->drawRound($round, $black);
+
+                  // Добавляем горизонтальные линии после каждого раунда, кроме последнего
+                  if ($round < $this->calculateRoundsCount() - 1) {
+                        $this->drawHorizontalConnectorLines($round, $black);
+                  }
+            }
+      }
+
+      private function drawHorizontalConnectorLines(int $round, int $black): void
+      {
+            $nextRound = $this->gridData[$round + 1];
+
+            for ($i = 0; $i < count($nextRound); $i++) {
+                  $match1X = (int) ($this->gridData[$round][$i * 2]['x'] + $this->matchWidth / 2);
+                  $match2X = (int) ($this->gridData[$round][$i * 2 + 1]['x'] + $this->matchWidth / 2);
+                  $lineY = (int) ($this->gridData[$round][$i * 2]['y'] + $this->matchHeight + $this->padding * 2);
+
+                  imageline(
+                        $this->image,
+                        $match1X,
+                        $lineY,
+                        $match2X,
+                        $lineY,
+                        $black
+                  );
+
+                  $lineDownX = floor(($match1X + $match2X) / 2);
+                  $line2Y = $lineY + $this->matchHeight;
+
+                  imageline(
+                        $this->image,
+                        $lineDownX,
+                        $lineY,
+                        $lineDownX,
+                        $line2Y,
+                        $black
+                  );
             }
       }
 
@@ -106,21 +167,23 @@ class GridGenerator
                   $black
             );
 
-            imagestring(
+            imagettftext(
                   $this->image,
                   $this->fontSize,
-                  (int) ($match['x'] + $this->padding / 4),
-                  (int) ($match['y'] + $this->padding / 4),
-                  $this->generateMatchText($match),
-                  $black
+                  0,
+                  (int) ($match['x'] + $this->padding),
+                  (int) ($match['y'] + $this->padding),
+                  $black,
+                  $this->fontFile,
+                  $this->generateMatchText($match)
             );
       }
 
       private function generateMatchText(array $match): string
       {
-            $text = "Match {$match['matchNumber']}\n";
+            $text = "             " . "Match {$match['matchNumber']}\n\n";
             if ($match['previousMatch1'] !== null) {
-                  $text .= "Winner {$match['previousMatch1']}";
+                  $text .= "  " . "Winner {$match['previousMatch1']}";
             }
             if ($match['previousMatch2'] !== null) {
                   $text .= " vs Winner {$match['previousMatch2']}";
@@ -135,10 +198,11 @@ class GridGenerator
                   (int) ($match['x'] + $this->matchWidth / 2),
                   (int) ($match['y'] + $this->matchHeight),
                   (int) ($match['x'] + $this->matchWidth / 2),
-                  (int) ($match['y'] + $this->matchHeight + $this->padding),
+                  (int) ($match['y'] + $this->matchHeight + $this->padding * 2),
                   $black
             );
       }
+
 
       private function calculateRoundsCount(): int
       {
